f02e4963a99856aeaace4b4a2f8e30d94b94ecf5
GVOne-blood <dta150904@gmail.com>
2024-08-04 10:46:52 +0700

    BTVN4

--- BTVN_B4/bai4.py
+++ BTVN_B4/bai4.py
@@ -2,6 +2,10 @@ n = int(input('nhap n : '))
 a = set(map(int, input('nhap a : ').split()[:n]))
 k = int(input('nhap k : '))
 b = set()
+a = list(a)
+a.sort()
+a = set(a)
+print(a)
 sum = 0
 for i in a : 
     sum += i
