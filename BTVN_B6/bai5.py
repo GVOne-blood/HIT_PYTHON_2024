665abe3a0b03530256518b5e7fecfd1fc13f61d8
GVOne-blood <140496174+GVOne-blood@users.noreply.github.com>
2024-08-19 10:41:48 +0700

    Add files via upload

--- BTVN_B6/bai5.py
+++ BTVN_B6/bai5.py
@@ -1,10 +1,14 @@
 phone_number = input("Nhập số điện thoại: ")
 numbers = ['0', '1', '2', '3', '4', '5' ,'6' ,'7' ,'8' ,'9']
-l = []
-for i in range(len(phone_number)):
-    if phone_number[i] in numbers:
-      l.append(phone_number[i])
-if len(l) != 10 or l[0] != "0":
-    print('Invalid phone number')
-else :
-    print('Phone number is : ',''.join(map(str, l)))
+def format_phone_number(phone_number):
+    l = []
+    for i in range(len(phone_number)):
+        if phone_number[i] in numbers:
+            l.append(phone_number[i])
+    if len(l) != 10 or l[0] != "0":
+        return 'Invalid phone number'
+    else :
+        return ''.join(map(str, l))
+#
+print(format_phone_number(phone_number))
+
